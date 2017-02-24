f5661b14f03da423812bfb443f59dbf166304928
SkaviCos <skavinski_90@mail.ru>
2017-02-24 21:14:11 +0300

    Добавлен новый модуль

---
+++ sandbox/src/test/java/ru/TrainingJava/sandbox/SquareTest.java
@@ -0,0 +1,16 @@
+package ru.TrainingJava.sandbox;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTest {
+@Test
+    public void testArea(){
+
+        Square s = new Square(5);
+        //assert s.area() != 26; passed
+        //Assert.assertEquals(25, s.area()); failed, because 25  isn`t double number
+        Assert.assertEquals(25.0, s.area());
+    }
+
+
+}
