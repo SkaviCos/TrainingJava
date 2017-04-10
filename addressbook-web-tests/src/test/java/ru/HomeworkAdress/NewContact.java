83452210819b9c7600acc0d1d942ae3a72a79729
SkaviCos <skavinski_90@mail.ru>
2017-04-10 21:52:48 +0300

    HomeWork

--- addressbook-web-tests/src/test/java/ru/HomeworkAdress/NewContact.java
+++ addressbook-web-tests/src/test/java/ru/HomeworkAdress/NewContact.java
@@ -1,6 +1,5 @@
 package ru.HomeworkAdress;
 
-
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -15,7 +14,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 import static org.openqa.selenium.OutputType.*;
 
-public class NewGroup {
+public class NewContact {
     FirefoxDriver wd;
     
     @BeforeMethod
@@ -25,20 +24,26 @@ public class NewGroup {
     }
     
     @Test
-    public void NewGroup() {
+    public void testNewContact() {
         wd.get("http://localhost/addressbook/");
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).sendKeys("admin");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.name("new")).click();
-        wd.findElement(By.name("group_name")).sendKeys("NewGroup");
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).sendKeys("1");
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).sendKeys("1");
-        wd.findElement(By.name("submit")).click();
-        wd.findElement(By.linkText("groups")).click();
+        wd.findElement(By.linkText("add new")).click();
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).sendKeys("Test1");
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).sendKeys("Test2");
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).sendKeys("Ska");
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).sendKeys("Yandex");
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).sendKeys("+74952655648");
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+        wd.findElement(By.xpath("//div/div[3]/ul/li[2]/a")).click();
     }
     
     @AfterMethod
